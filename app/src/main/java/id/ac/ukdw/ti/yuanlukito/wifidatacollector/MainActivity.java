aabed467c7fb5ba1a371730c5e4303828d3ff156
Yuan Lukito <yuanlukito@ti.ukdw.ac.id>
2015-04-16 15:00:20 +0700

    MainActivity GUI Components
    Code Template for MainActivity, property named all GUI components

--- app/src/main/java/id/ac/ukdw/ti/yuanlukito/wifidatacollector/MainActivity.java
+++ app/src/main/java/id/ac/ukdw/ti/yuanlukito/wifidatacollector/MainActivity.java
@@ -1,16 +1,70 @@
 package id.ac.ukdw.ti.yuanlukito.wifidatacollector;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    //Broadcast Receiver (WiFi Scanning)
+    private WifiManager wifiManager;
+    private IntentFilter intentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+    private BroadcastReceiver broadcastReceiver;
+
+    //list view wifi stations
+    private ListView listViewWifiStations;
+    private List<ScanResult> wifiScanResults;
+    private ArrayAdapter<String> wifiStationsAdapter;
+
+    //button scan
+    private Button buttonScan;
+    private Button buttonSave;
+    private Button buttonManageData;
+
+    //spinner location
+    private Spinner spinnerLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //initialize wifi manager and enable wifi
+        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if(!wifiManager.isWifiEnabled())
+            wifiManager.setWifiEnabled(true);
+
+        //receive wifi scan results
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                wifiScanResults = wifiManager.getScanResults();
+
+                //update list view datasource
+            }
+        };
+
+        //register broadcast receiver
+        registerReceiver(broadcastReceiver, intentFilter);
+
+        //button handling
+
+
+        //list view initialization
+
         setContentView(R.layout.activity_main);
     }
 
